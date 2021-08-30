924c853b70a38b29511fae47bd6559a1479904ec
SepShr <sshar190@uottawa.ca>
2021-08-30 19:40:11 -0400

    test_collaborateArchive.py added.

---
+++ tests/test_collaborateArchive.py
@@ -0,0 +1,16 @@
+from copy import Error
+from unittest.case import TestCase
+from deap import base, creator
+from CCEA import collaborateArchive
+import unittest
+
+class Test_TestCollaborateArchive(unittest.TestCase):
+    def test_collaborateArchive_testInput1(self):
+        # a = [creator.Scenario([1])]
+        a = [creator.Scenario([1]), creator.Scenario(['a'])]
+        b = creator.OutputMLC([2])
+        complete_solution_set = collaborateArchive(a, b, creator.Individual)
+        self.assertEqual(complete_solution_set, [[[1], 2], [['a'], 2]])
+
+if __name__=='__main__':
+    unittest.main()
