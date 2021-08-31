0f124e180de053992d8a80e681919eaaa6b22830
SepShr <sshar190@uottawa.ca>
2021-08-31 10:36:21 -0400

    unused lines removed from test_collaborateArchive

--- tests/test_collaborateArchive.py
+++ tests/test_collaborateArchive.py
@@ -1,12 +1,9 @@
-from copy import Error
-from unittest.case import TestCase
-from deap import base, creator
+from deap import creator
 from CCEA import collaborateArchive
 import unittest
 
 class Test_TestCollaborateArchive(unittest.TestCase):
     def test_collaborateArchive_testInput1(self):
-        # a = [creator.Scenario([1])]
         a = [creator.Scenario([1]), creator.Scenario(['a'])]
         b = creator.OutputMLC([2])
         complete_solution_set = collaborateArchive(a, b, creator.Individual)
