91e8d8c17f1bd14f0317991ce98f97848b6ddcd8
SepShr <sshar190@uottawa.ca>
2021-09-01 16:13:16 -0400

    test_collaborateArchive() updated.

--- tests/test_collaborateArchive.py
+++ tests/test_collaborateArchive.py
@@ -6,7 +6,9 @@ class Test_TestCollaborateArchive(unittest.TestCase):
     def test_collaborateArchive_testInput1(self):
         a = [creator.Scenario([1]), creator.Scenario(['a'])]
         b = creator.OutputMLC([2])
-        complete_solution_set = collaborateArchive(a, b, creator.Individual)
+        cls = creator.Individual
+        fcls = creator.Scenario
+        complete_solution_set = collaborateArchive(a, b, cls, fcls)
         self.assertEqual(complete_solution_set, [[[1], 2], [['a'], 2]])
 
 if __name__=='__main__':
