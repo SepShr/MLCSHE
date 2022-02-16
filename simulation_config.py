b9069abdcd825be1c6ff62b37e8277bf525b17dc
SepShr <57507176+SepShr@users.noreply.github.com>
2022-02-15 23:39:32 -0500

    relative paths added

--- simulation_config.py
+++ simulation_config.py
@@ -1,6 +1,10 @@
 base_directory = ""
+<<<<<<< HEAD
 container_name = "pylot_main"
 simulation_duration = 100  # Simulation duration in seconds.
+=======
+container_name = "pylot"
+>>>>>>> e0202fe40f620bbd2b756a242622aa152604179a
 base_config_file_name = "base_config.conf"
 config_source_path = "/mlcshe_config.conf"
 config_destination_path = "/home/erdos/workspace/pylot/configs/mlcshe/mlcshe_config.conf"
