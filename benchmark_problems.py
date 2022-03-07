f88a17bd635ff7978e36e04f7a627856a91cb2c9
SepShr <57507176+SepShr@users.noreply.github.com>
2022-03-07 14:49:03 -0500

    refactored

--- benchmark_problems.py
+++ benchmark_problems.py
@@ -5,73 +5,79 @@
 
 # Define the problem's joint fitness function.
 
-# def problem_joint_fitness(x, y):
-#     """This is the problem-specific joint fitness evaluation.
-#     """
-#     # The MTQ problem.
+from math import cos, sqrt
 
-#     cf = 10  # Correction factor that controls the granularity of x and y.
 
-#     h_1 = 50
-#     x_1 = 0.75
-#     y_1 = 0.75
-#     s_1 = 1.6
-#     f_1 = h_1 * \
-#         (1 - ((16.0/s_1) * pow((x/cf - x_1), 2)) -
-#          ((16.0/s_1) * pow((y/cf - y_1), 2)))
+def problem_joint_fitness(x, y):
+    """This is the problem-specific joint fitness evaluation.
+    """
+    # The MTQ problem.
 
-#     h_2 = 150
-#     x_2 = 0.25
-#     y_2 = 0.25
-#     s_2 = 1.0/32.0
-#     f_2 = h_2 * \
-#         (1 - ((16.0/s_2) * pow((x/cf - x_2), 2)) -
-#          ((16.0/s_2) * pow((y/cf - y_2), 2)))
+    cf = 10  # Correction factor that controls the granularity of x and y.
 
-#     return max(f_1, f_2)
+    h_1 = 50
+    x_1 = 0.75
+    y_1 = 0.75
+    s_1 = 1.6
+    f_1 = h_1 * \
+        (1 - ((16.0/s_1) * pow((x/cf - x_1), 2)) -
+         ((16.0/s_1) * pow((y/cf - y_1), 2)))
 
-# def problem_joint_fitness(x, y):
-#     """This is the problem-specific joint fitness evaluation.
-#     """
-#     # The Griewangk domain.
+    h_2 = 150
+    x_2 = 0.25
+    y_2 = 0.25
+    s_2 = 1.0/32.0
+    f_2 = h_2 * \
+        (1 - ((16.0/s_2) * pow((x/cf - x_2), 2)) -
+         ((16.0/s_2) * pow((y/cf - y_2), 2)))
 
-#     cf = 5.0  # Correction factor that controls the granularity of x and y.
+    return max(f_1, f_2)
 
-#     x_bar = 10.24 * (x / cf) - 5.12
-#     y_bar = 10.24 * (y / cf) - 5.12
 
-#     f = -1.0 - (pow(x_bar, 2) / 4000) - (pow(y_bar, 2) / 4000) + \
-#         cos(x_bar) * cos(y_bar/(sqrt(2)))
+def problem_joint_fitness(x, y):
+    """This is the problem-specific joint fitness evaluation.
+    """
+    # The Griewangk domain.
 
-#     return f
+    cf = 5.0  # Correction factor that controls the granularity of x and y.
 
-# def problem_joint_fitness(x, y):
-#     """This is the problem-specific joint fitness evaluation.
-#     """
-#     # The OneRidge domain.
+    x_bar = 10.24 * (x / cf) - 5.12
+    y_bar = 10.24 * (y / cf) - 5.12
 
-#     cf = 1.0  # Correction factor that controls the granularity of x and y.
+    f = -1.0 - (pow(x_bar, 2) / 4000) - (pow(y_bar, 2) / 4000) + \
+        cos(x_bar) * cos(y_bar/(sqrt(2)))
 
-#     # if x > 1:
-#     #     x = 1
-#     # if y > 1:
-#     #     y = 1
+    return f
 
-#     f = 1 + 2 * min(x, y) - max(x, y)
 
-#     return f
+def problem_joint_fitness(x, y):
+    """This is the problem-specific joint fitness evaluation.
+    """
+    # The OneRidge domain.
 
-# def problem_joint_fitness(x, y):
-#     """This is the problem-specific joint fitness evaluation.
-#     """
-#     # The Booth domain.
+    cf = 1.0  # Correction factor that controls the granularity of x and y.
 
-#     cf = 1.0  # Correction factor that controls the granularity of x and y.
+    # if x > 1:
+    #     x = 1
+    # if y > 1:
+    #     y = 1
 
-#     x_bar = 10.24 * x - 5.12
-#     y_bar = 10.24 * y - 5.12
+    f = 1 + 2 * min(x, y) - max(x, y)
 
-#     f = -1.0 * pow((x_bar + 2.0 * y_bar - y), 2) - \
-#         pow((2 * x_bar + y_bar - 5.0), 2)
+    return f
 
-#     return f
+
+def problem_joint_fitness(x, y):
+    """This is the problem-specific joint fitness evaluation.
+    """
+    # The Booth domain.
+
+    cf = 1.0  # Correction factor that controls the granularity of x and y.
+
+    x_bar = 10.24 * x - 5.12
+    y_bar = 10.24 * y - 5.12
+
+    f = -1.0 * pow((x_bar + 2.0 * y_bar - y), 2) - \
+        pow((2 * x_bar + y_bar - 5.0), 2)
+
+    return f
