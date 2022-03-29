bfc0697998e1636b296470588acf7b2802987177
SepShr <57507176+SepShr@users.noreply.github.com>
2022-03-29 12:15:13 -0400

    trying to create a module for benchmarkproblems

---
+++ benchmark/griegwangk/problem.py
@@ -0,0 +1,65 @@
+"""
+Set of variables that define the `problem` structure, especially `creator`
+and `toolbox`.
+
+This is the only file that a user has to modify for a given problem to
+solve.
+"""
+from deap import base, creator, tools
+
+import search_config as cfg
+from benchmark_problems import (joint_fitness_booth, joint_fitness_griegwangk,
+                                joint_fitness_mtq, joint_fitness_oneridge)
+from problem_utils import initialize_mlco, mutate_mlco, mutate_scenario
+from src.utils.utility import initialize_hetero_vector
+
+# FIXME: This should be imported from problem_utils.py
+
+scen_pop_size = cfg.scenario_population_size  # Size of the scenario population
+mlco_pop_size = cfg.mlco_population_size  # Size of the MLC output population
+min_distance = cfg.min_distance  # Minimum distance between members of an archive
+
+# The list of lower and upper limits for enumerationed types in sceanrio.
+scen_enumLimits = cfg.scenario_enumLimits
+
+# Create fitness and individual datatypes.
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))   # Maximization
+creator.create("Individual", list, fitness=creator.FitnessMax)  # Maximization
+# creator.create("FitnessMin", base.Fitness, weights=(-1.0,))  # Minimization
+# creator.create("Individual", list, fitness=creator.FitnessMin)  # Minimization
+creator.create("Scenario", creator.Individual)
+creator.create("OutputMLC", creator.Individual)
+
+toolbox = base.Toolbox()
+
+# Define functions and register them in toolbox.
+toolbox.register(
+    "scenario", initialize_hetero_vector,
+    class_=creator.Scenario, limits=scen_enumLimits
+)
+
+toolbox.register(
+    "mlco", initialize_hetero_vector,
+    creator.OutputMLC, limits=scen_enumLimits
+)
+
+toolbox.register(
+    "popScen", tools.initRepeat, list,
+    toolbox.scenario, n=scen_pop_size
+)
+toolbox.register(
+    "popMLCO", tools.initRepeat, list,
+    toolbox.mlco, n=mlco_pop_size
+)
+
+toolbox.register("problem_jfit", joint_fitness_mtq)
+
+toolbox.register(
+    "select", tools.selTournament,
+    tournsize=cfg.tournament_selection, fit_attr='fitness'
+)
+
+toolbox.register("crossover", tools.cxUniform)
+
+toolbox.register("mutate_mlco", mutate_mlco)
+toolbox.register("mutate_scenario", mutate_scenario)
---
+++ run_iccea_mtq.py
@@ -0,0 +1,49 @@
+"""
+iCCEA Runner.
+"""
+from src.main.ICCEA import ICCEA
+from src.utils.utility import setup_logger
+from benchmark.mtq import problem
+from simulation_runner import Simulator
+
+import search_config as cfg
+
+
+# NOTE: ICCEA is an algorithm, which is independent of a problem structure
+# FIXME: Refactor enumLimits
+
+
+def main():
+    # Instantiate simulator instance.
+    simulator = Simulator()
+
+    solver = ICCEA(
+        creator=problem.creator,
+        toolbox=problem.toolbox,
+        simulator=simulator,
+        # more parameters can be added to better define the problem
+        first_population_enumLimits=cfg.enumLimits
+    )
+
+    hyperparameters = [
+        cfg.min_distance,
+        cfg.tournament_selection,
+        cfg.crossover_probability,
+        cfg.guassian_mutation_mean,
+        cfg.guassian_mutation_std,
+        cfg.guassian_mutation_probability,
+        cfg.integer_mutation_probability,
+        cfg.bitflip_mutation_probability
+    ]
+
+    # Setup logger.
+    setup_logger(file_name='CCEA', file_log_level='DEBUG',
+                 stream_log_level='INFO')
+
+    # User does not need to modify anything but `problem.py`
+    solution = solver.solve(
+        max_gen=cfg.number_of_generations, hyperparameters=hyperparameters, seed=cfg.random_seed)
+
+
+if __name__ == "__main__":
+    main()
