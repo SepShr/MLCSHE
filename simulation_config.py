9228a1080bbe1ba9082a85355bdae43de2e962a1
SepShr <57507176+SepShr@users.noreply.github.com>
2023-01-12 22:37:01 -0500

    scripts folder moved to pylot

--- simulation_config.py
+++ simulation_config.py
@@ -1,10 +1,9 @@
 base_directory = ""
-script_directory = 'scripts/'
+script_directory = 'pylot/scripts/'
 container_name = "pylot"
 carla_timeout = 10
 pylot_timeout = 20
-# simulation_duration = 350  # Simulation duration in seconds.
-simulation_duration = 30  # Simulation duration in seconds.
+simulation_duration = 350  # Simulation duration in seconds.
 mlco_file_name = 'mlco_list.pkl'
 mlco_destination_path = '/home/erdos/workspace/pylot/dependencies/mlco/mlco_list.pkl'
 base_config_file = "configs/base_config.conf"
@@ -16,7 +15,7 @@ simulation_results_source_directory = "/home/erdos/workspace/results/"
 simulation_results_destination_path = "./results/"
 # The path to the finished.txt file in the container.
 finished_file_path = '/home/erdos/workspace/results/finished.txt'
-pylot_runner_path = '/scripts/run_pylot.sh'
+pylot_runner_path = 'pylot/scripts/run_pylot.sh'
 carla_runner_path = "/home/erdos/workspace/pylot/scripts/run_simulator.sh"
 container_img_name = 'sepshr/pylot'
 docker_repo_tag = '2.1'
